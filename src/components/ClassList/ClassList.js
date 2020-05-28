3822008ad3ba945390560488099dcd6d3d3873cb
palmytree <jeff@palmytree.com>
2020-05-28 17:53:44 -0500

    finished black diamond

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,18 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default class ClassList extends Component {
-  constructor() {
-    super()
-    
-  }
+	constructor() {
+		super()
+		this.state = {
+			students: []
+		}
+	}
 
-  render() {
-    return (
-      <div className="box">
-        <h1></h1>
-        <h2>ClassList:</h2>
+	componentDidMount() {
+		const { params } = this.props.match
 
-      </div>
-    )
-  }
+		axios
+			.get(`http://localhost:3005/students?class=${params.class}`)
+			.then(res => {
+				console.log(res)
+				this.setState({ students: res.data })
+			})
+			.catch(err => console.log(err))
+	}
+
+	render() {
+		const { params } = this.props.match,
+			{ students } = this.state
+		return (
+			<div className='box'>
+				<h1>{params.class}</h1>
+				<h2>ClassList:</h2>
+				{students.map(student => (
+					<Link to={`/student/${student.id}`} key={student.id}>
+						<h3>{`${student.first_name} ${student.last_name}`}</h3>
+					</Link>
+				))}
+				<button onClick={this.props.history.goBack}>Back</button>
+			</div>
+		)
+	}
 }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Student extends Component {
-  constructor() {
-    super()
+	constructor() {
+		super()
+		this.state = {
+			studentInfo: {}
+		}
+	}
 
-  }
+	componentDidMount() {
+		const { params } = this.props.match
 
-  render() {
-    return (
-      <div className="box">
-        <h1>Student</h1>
-      </div>
-    )
-  }
+		axios
+			.get(`http://localhost:3005/students/${params.id}`)
+			.then(res => this.setState({ studentInfo: res.data }))
+			.catch(err => console.log(err))
+	}
+
+	render() {
+		const { studentInfo } = this.state
+		return (
+			<div className='box'>
+				<h1>Student: {`${studentInfo.first_name} ${studentInfo.last_name}`}</h1>
+				<h3>Grade: {studentInfo.grade}</h3>
+				<h3>Email: {studentInfo.email}</h3>
+				<button onClick={this.props.history.goBack}>Back</button>
+				
+			</div>
+		)
+	}
 }
